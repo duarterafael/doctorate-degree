c3adfd8a2f44dea8ea12517b0fdf93a3b994c61a
rbd <rbd@cin.ufpe.br>
2019-07-27 00:55:21 -0300

    show model backgout

--- heat-map/index.php
+++ heat-map/index.php
@@ -8,6 +8,7 @@
        <style>
            body { text-align: center; font: 16px/1.4 "Helvetica Neue", Arial, sans-serif; }
            a { color: #0077ff; }
+           .model {background-image: url("model-sample.png"); width: 100%; height: 100%}
            .container { width: 1000px; height: 600px; margin: 0 auto; position: relative; border: 1px solid #ccc; }
            .options { position: absolute; top: 0; right: 0; padding: 10px; background: rgba(255,255,255,0.6);
                border-bottom: 1px solid #ccc; border-left: 1px solid #ccc; line-height: 1; }
@@ -17,19 +18,16 @@
        </style>
    </head>
    <body>
-   <!--p>
-       <strong>simpleheat</strong> is a tiny and fast JS heatmap library.
-       More on <a href="https://github.com/mourner/simpleheat">mourner / simpleheat</a>
-       <iframe class="ghbtns" src="http://ghbtns.com/github-btn.html?user=mourner&amp;repo=simpleheat&amp;type=watch&amp;count=true"
-     allowtransparency="true" frameborder="0" scrolling="0" width="90" height="20"></iframe>
-   </p-->
-   <div class="container">
-       <div class="options">
-           <label>Radius </label><input type="range" id="radius" value="25" min="10" max="50" /><br />
-           <label>Blur </label><input type="range" id="blur" value="15" min="10" max="50" />
-       </div>
-       <canvas id="canvas" width="1000" height="600"></canvas>
+   <div class="model">
+    <div class="container">
+        <div class="options">
+            <label>Radius </label><input type="range" id="radius" value="25" min="10" max="50" /><br />
+            <label>Blur </label><input type="range" id="blur" value="15" min="10" max="50" />
+        </div>
+        <canvas id="canvas" width="1260" height="1820"></canvas>
+    </div>
    </div>
+  
    <script src="simpleheat.js"></script>
    <script src="heat.map.js"></script>
    </body>
